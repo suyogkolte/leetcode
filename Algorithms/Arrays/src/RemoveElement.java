454b317dbd52cad125871c81d87177eed739b8c9
suyogkolte <suyogkolte1989@gmail.com>
2024-01-08 21:36:13 -0500

    Create RemoveElement.java
    Code to remove element

---
+++ Algorithms/Arrays/src/RemoveElement.java
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/remove-element/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+
+        while(i <= j)
+        {
+            if(nums[i] != val)
+                i++;
+            else
+            {
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+                j--;
+            }
+        }
+        return i;
+    }
+}
