4d5eda912ca017250eedf103395090810ec64405
suyogkolte <suyogkolte1989@gmail.com>
2024-01-10 19:30:52 -0500

    Create MatrixDiagonalSum.java
    Code for MatrixDiagonalSum

---
+++ Algorithms/Arrays/src/MatrixDiagonalSum.java
@@ -0,0 +1,19 @@
+// Source : https://leetcode.com/problems/matrix-diagonal-sum/description/
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int ans = 0;
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(i == j || (cols-1-i) == j)
+                    ans = ans + mat[i][j];
+            }
+        }
+        return ans;
+    }
+}
