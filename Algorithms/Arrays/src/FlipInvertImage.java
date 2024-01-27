f8169030452fd8d1a1412a7c06d37bd355a9eee8
suyogkolte <suyogkolte1989@gmail.com>
2024-01-27 07:01:51 -0500

    Create FlipInvertImage.java
    Code for flipping and inverting an image

---
+++ Algorithms/Arrays/src/FlipInvertImage.java
@@ -0,0 +1,33 @@
+// Source : https://leetcode.com/problems/flipping-an-image/description/
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int rows = image.length;
+        int cols = image[0].length;
+
+        // Flip horizontally
+        for(int i = 0; i < rows; i++)
+        {
+            int j = 0;
+            int k = cols-1;
+
+            while(j <= k)
+            {
+                int tmp = image[i][j];
+                image[i][j] = image[i][k];
+                image[i][k] = tmp;
+                j++;
+                k--;
+            }
+        }
+
+        // Invert
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                image[i][j] = (image[i][j] == 0) ? 1 : 0;
+            }
+        }
+        return image;
+    }
+}
