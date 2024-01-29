5797bb2f1bd26282b3611725e39ffcc63ff69d0f
suyogkolte <suyogkolte1989@gmail.com>
2024-01-29 07:25:52 -0500

    Create GetSubstrings.java
    Code to get all substrings in a string

---
+++ Algorithms/Strings/src/GetSubstrings.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class GetSubstrings {
+    public static void main(String[] args) {
+        String str = "code";
+
+        ArrayList<String> res = getSubstrings(str);
+        for(String s : res){
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> getSubstrings(String str){
+        ArrayList<String> resList = new ArrayList<>();
+        int len = str.length();
+
+        for(int startIdx = 0; startIdx < len; startIdx++){
+            for(int endIdx = startIdx + 1; endIdx <= len; endIdx++){
+                resList.add(str.substring(startIdx, endIdx));
+            }
+        }
+        return resList;
+    }
+}
