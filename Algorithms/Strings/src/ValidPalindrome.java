cb19e5434364ee79731f39b3c1d2e460eade329f
suyogkolte <suyogkolte1989@gmail.com>
2024-01-31 06:08:42 -0500

    Create ValidPalindrome.java
    Code to check if a given string is palindrome or not

---
+++ Algorithms/Strings/src/ValidPalindrome.java
@@ -0,0 +1,18 @@
+// Source : https://leetcode.com/problems/valid-palindrome/description/
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        s = s.trim(); // remove leading and trailing whitespace
+        s = s.replaceAll("[^A-Za-z0-9]", ""); //remove all non-alphanumeric characters
+        s = s.toLowerCase();
+
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
