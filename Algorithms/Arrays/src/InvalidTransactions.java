ab0b74c0808ae3422574a428e7c3be802af0d32f
suyogkolte <suyogkolte1989@gmail.com>
2024-02-04 08:03:28 -0500

    Create InvalidTransactions.java
    Code to check if the given transactions are valid or not

---
+++ Algorithms/Arrays/src/InvalidTransactions.java
@@ -0,0 +1,80 @@
+// Source : https://leetcode.com/problems/invalid-transactions/description/
+
+import java.util.*;
+
+public class InvalidTransactions {
+    public class Transaction{
+        String name;
+        int time;
+        String city;
+        String trans;
+
+        public Transaction(String name, int time, String city, String trans){
+            this.name = name;
+            this.time = time;
+            this.city = city;
+            this.trans = trans;
+        }
+    }
+
+    public List<String> invalidTransactions(String[] transactions) {
+        //We use set to avoid duplicates since we can reject the
+        //same transaction for multiple reasons
+        Set<String> out = new HashSet<>();
+
+        //We can have duplicate transactions in the intput.
+        //We want to output multiple in that case.
+        Map<String, Integer> sameTransactionCount = new HashMap<>();
+
+        //We will keep the track of transactions per person.
+        Map<String, List<Transaction>> perPerson = new HashMap<>();
+
+        for(String trans : transactions){
+            //Update count
+            sameTransactionCount.put(trans, sameTransactionCount.getOrDefault(trans, 0) + 1);
+
+            //Extract data from transactions
+            String[] split = trans.split(",");
+
+            String name = split[0];
+            int time = Integer.valueOf(split[1]);
+            int amount = Integer.valueOf(split[2]);
+            String city = split[3];
+
+            if(amount > 1000){
+                //reject transaction because amount is to large
+                out.add(trans);
+            }
+
+            List<Transaction> otherTransactions = perPerson.get(name);
+
+            if(otherTransactions == null){
+                //first transaction for this person
+                otherTransactions = new ArrayList<>();
+                otherTransactions.add(new Transaction(name, time, city, trans));
+                perPerson.put(name, otherTransactions);
+            } else{
+                for(Transaction transa : otherTransactions){
+                    if(!transa.city.equals(city) &&
+                            Math.abs(transa.time - time) <= 60){
+                        //reject both transaction because they are in
+                        //different cities within 60 mins of each other
+                        out.add(transa.trans);
+                        out.add(trans);
+                    }
+                }
+
+                otherTransactions.add(new Transaction(name, time, city, trans));
+            }
+        }
+
+        List<String> output =  new ArrayList<>();
+        for(String trans : out) {
+            for(int i = 0; i < sameTransactionCount.get(trans); i++) {
+                output.add(trans);
+            }
+        }
+
+        return output;
+    }
+}
