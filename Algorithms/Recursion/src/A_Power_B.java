d048afa86108043f02c3168b1af4a4c534126336
suyogkolte <suyogkolte1989@gmail.com>
2024-02-05 07:38:23 -0500

    Update A_Power_B.java
    Refactored code for calculating a power b

--- Algorithms/Recursion/src/A_Power_B.java
+++ Algorithms/Recursion/src/A_Power_B.java
@@ -1,29 +1,37 @@
 // Given 2 integer values a and b, write a recursive function to implement a power b
 public class A_Power_B {
     public static void main(String[] args) {
-        int a = 4;
-        int b = 3;
+        float res = 0;
+        int a = 3;
+        int b = -3;
+
         if(a == 0)
             System.out.println(a + " power " + b + " is " + a);
         else{
-            int res = power(a, b);
-            System.out.println(a + " power " + b + " is " + res);
+            res = power(a, b);
+            if(b > 0)
+                System.out.println(a + " power " + b + " is " + res);
+            else{
+                res = 1 / power(a, b);
+                System.out.println(a + " power " + b + " is " + res);
+            }
+
         }
     }
 
-    public static int power(int a, int b){
+    public static float power(int a, int b){
         //base case
         if(b == 0)
             return 1;
 
-        int ans = 1;
+        float ans = 1;
         if(b > 0){
             //self work and recursive call
             ans = a * power(a, b - 1);
         }
         else{
             //self work and recursive call
-            ans = power(a, b + 1) / a;
+            ans = a * power(a, b + 1);
         }
 
         return ans;
