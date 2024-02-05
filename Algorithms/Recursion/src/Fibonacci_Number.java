5d64b5a279685990e3b7bfa97ed937a770d2d39e
suyogkolte <suyogkolte1989@gmail.com>
2024-02-05 17:10:20 -0500

    Create Fibonacci_Number.java
    Code to calculate the nth fibonacci number

---
+++ Algorithms/Recursion/src/Fibonacci_Number.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/fibonacci-number/description/
+public class Fibonacci_Number {
+    public int fib(int n) {
+        //Base case
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        //recursive task
+        int tmp1 = fib(n - 1);
+        int tmp2 = fib(n - 2);
+
+        //Self work
+        return tmp1 + tmp2;
+    }
+}
