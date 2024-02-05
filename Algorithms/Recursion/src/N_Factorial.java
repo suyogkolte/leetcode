d7790ed0ea43e913c39f19280176f9adcbe15de1
suyogkolte <suyogkolte1989@gmail.com>
2024-02-04 19:53:18 -0500

    Create N_Factorial.java
    Code to calculate n factorial

---
+++ Algorithms/Recursion/src/N_Factorial.java
@@ -0,0 +1,20 @@
+//Calculate n factorial
+public class N_Factorial {
+    public static void main(String[] args) {
+        int n = 2;
+        int res = calcFactorial(n);
+        System.out.println(n + " factorial is " + res);
+    }
+
+    public static int calcFactorial(int n){
+        //Base condition
+        if(n <= 1)
+            return 1;
+
+        //Recursion
+        int tmp = calcFactorial(n - 1);
+
+        //Self work
+        return n * tmp;
+    }
+}
