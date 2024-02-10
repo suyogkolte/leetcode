e07c0ffbb1ad3ed4e6dc35c2fc797457ced2b9bb
suyogkolte <suyogkolte1989@gmail.com>
2024-02-10 07:45:51 -0500

    Create Contents_Between_Parenthesis.java
    Code to return the content between the parenthesis in a given string str

---
+++ Algorithms/Recursion/src/Contents_Between_Parenthesis.java
@@ -0,0 +1,35 @@
+/*
+Take as input string str. The string contains a single pair of parenthesis ""(...)
+Write a recursive function which returns the content between the parenthesis
+e.g. for I/P: "xyz(abc)def"
+         O/P: "abc"
+
+Print the value returned
+*/
+
+public class Contents_Between_Parenthesis {
+    public static void main(String[] args) {
+        String str = "xyz(abc)def";
+        StringBuilder strRes = new StringBuilder();
+        boolean bStartPar = false;
+
+        parenthesisSubstring(str, strRes, 0, bStartPar);
+        System.out.println("For input: " + str + " , output is " + strRes.toString());
+    }
+
+    private static void parenthesisSubstring(String str, StringBuilder strRes, int idx, boolean bStartingParenthesis){
+        //Base case
+        if(str.charAt(idx) == ')' || idx == str.length())
+            return;
+
+        //Self work
+        if(bStartingParenthesis)
+            strRes.append(str.charAt(idx));
+        if(str.charAt(idx) == '(')
+            bStartingParenthesis = true;
+
+
+        //Recursive task
+        parenthesisSubstring(str, strRes, idx + 1, bStartingParenthesis);
+    }
+}
