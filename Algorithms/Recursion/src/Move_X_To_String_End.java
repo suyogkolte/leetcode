ca87cd01c6d80b52a276428f4c8249c2f9ff7022
suyogkolte <suyogkolte1989@gmail.com>
2024-02-10 07:21:44 -0500

    Create Move_X_To_String_End.java
    Code to move a char x to the end of given string str recursively

---
+++ Algorithms/Recursion/src/Move_X_To_String_End.java
@@ -0,0 +1,41 @@
+/*
+Take as input str, a string. Write a recursive function which moves all 'x' from the string to its end.
+e.g. I/P: abexexdexed
+     O/P: abeedeedxxx
+
+*/
+
+public class Move_X_To_String_End {
+    public static void main(String[] args) {
+        String str = "abexexdexed";
+        char ch = 'x';
+
+        StringBuilder strRes = new StringBuilder();
+        int count = 0;
+        count = moveToEnd(str, strRes, ch, 0, count);
+
+        while(count > 0){
+            strRes.append(ch);
+            count--;
+        }
+
+        System.out.println("I/P: " + str);
+        System.out.println("O/P: " + strRes.toString());
+    }
+
+    private static int moveToEnd(String str, StringBuilder strRes, char ch, int idx, int count){
+        //Base case
+        if(idx == str.length())
+            return count;
+
+        //Self work
+        if(str.charAt(idx) == ch)
+            count = count + 1;
+        else
+            strRes.append(str.charAt(idx));
+
+
+        //Recursive task
+        return moveToEnd(str, strRes, ch, idx + 1, count);
+    }
+}
