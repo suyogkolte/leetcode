b971834e56385ae616ad67602d037b0dd38a8bec
suyogkolte <suyogkolte1989@gmail.com>
2024-02-11 00:31:18 -0500

    Create Reverse_Array.java
    Code to recursively reverse an array

---
+++ Algorithms/Recursion/src/Reverse_Array.java
@@ -0,0 +1,35 @@
+/*
+Given an array of length N, write a recursive function which reverses the array
+*/
+
+public class Reverse_Array {
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 20, 30, 40, 50};
+
+        System.out.println("For I/P array: ");
+        for (int num : arr){
+            System.out.print(num + " ");
+        }
+
+        reverseArr(arr, 0, arr.length - 1);
+        System.out.println();
+        System.out.println("It's reverse is O/P: ");
+        for (int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void reverseArr(int[] arr, int startIdx, int endIdx){
+        //Base case
+        if(startIdx >= endIdx)
+            return;
+
+        //Self work
+        int tmp = arr[startIdx];
+        arr[startIdx] = arr[endIdx];
+        arr[endIdx] = tmp;
+
+        //Recursive task
+        reverseArr(arr, startIdx + 1, endIdx - 1);
+    }
+}
