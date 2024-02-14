76eb3fa73ba91d916592393648077cba57600e8e
suyogkolte <suyogkolte1989@gmail.com>
2024-02-14 16:47:52 -0500

    Create Words_Larger_Than_Input_String.java
    Code to print all possible words in dictionary order larger than the given string

---
+++ Algorithms/Recursion/src/Words_Larger_Than_Input_String.java
@@ -0,0 +1,33 @@
+/*
+Given an input string str, write a recursive function which prints all the words possible by rearranging
+the characters of this string which are in dictionary order larger than the given string
+*/
+
+public class Words_Larger_Than_Input_String {
+    public static void main(String[] args) {
+        String str = "abc";
+        String origStr = new String(str);
+        if(str.length() == 0)
+            System.out.println("");
+        else
+            printLargerWords(origStr, str, "");
+    }
+
+    public static void printLargerWords(String origInput, String input, String output) {
+        //Base case
+        if(input.length() == 0) {
+            if(output.charAt(0) > origInput.charAt(0))
+                System.out.println(output);
+            return;
+        }
+
+        if(!output.isEmpty() && output.charAt(0) > origInput.charAt(0)){
+            System.out.println(output);
+        }
+
+        for(int i = 0; i < input.length(); i++) {
+            String restOfInput = input.substring(0, i) + input.substring(i+1);
+            printLargerWords(origInput, restOfInput, output + input.charAt(i));
+        }
+    }
+}
