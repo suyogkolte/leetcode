6c62531af7adcdd4c3dc797295ddce98e616d6ca
suyogkolte <suyogkolte1989@gmail.com>
2024-02-24 20:45:43 -0500

    Update ReverseLinkedList.java
    Updated code to reverse a linked list

--- Algorithms/LinkedList/src/ReverseLinkedList.java
+++ Algorithms/LinkedList/src/ReverseLinkedList.java
@@ -1,18 +1,40 @@
 // Source : https://leetcode.com/problems/reverse-linked-list/
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.List;
+
+/** Definition for singly-linked list. */
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
 class ReverseLinkedList {
-    // Iterative solution
-    public ListNode reverseListIterative(ListNode head) {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        head.next = node1;
+        ListNode node2 = new ListNode(3);
+        node1.next = node2;
+        ListNode node3 = new ListNode(4);
+        node2.next = node3;
+
+        System.out.println("Original LL is: ");
+        printLinkedList(head);
+        System.out.println();
+
+        //System.out.println("Iteratively Reversed LL is:");
+        //printLinkedList(reverseListIterative(head));
+        //System.out.println();
+
+        System.out.println("Recursively Reversed LL is:");
+        printLinkedList(reverseListRecursive(head));
+
+    }
+
+    public static ListNode reverseListIterative(ListNode head) {
         if(head == null)
             return head;
 
@@ -29,8 +51,7 @@ class ReverseLinkedList {
         return prev;
     }
 
-    // Recursive solution
-    public ListNode reverseListRecursive(ListNode head) {
+    public static ListNode reverseListRecursive(ListNode head) {
         if(head == null || head.next == null)
         {
             return head;
@@ -41,5 +62,13 @@ class ReverseLinkedList {
         head.next.next = head;
         head.next = null;
         return p;
+    }
+
+    public static void printLinkedList(ListNode head) {
+        ListNode curr = head;
+        while(curr != null){
+            System.out.println(curr.val);
+            curr = curr.next;
+        }
     }
 }
