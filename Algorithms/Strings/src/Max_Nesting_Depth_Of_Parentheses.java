79644c7e928969741c94791a87b21697d22a91c0
suyogkolte <suyogkolte1989@gmail.com>
2024-03-04 11:55:58 -0500

    Create Max_Nesting_Depth_Of_Parentheses.java
    Code to find the max nesting depth of the parentheses

---
+++ Algorithms/Strings/src/Max_Nesting_Depth_Of_Parentheses.java
@@ -0,0 +1,26 @@
+//Source: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+
+public class Max_Nesting_Depth_Of_Parentheses {
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        int res = maxDepth(s);
+
+        System.out.println("The max depth is " + res);
+    }
+
+    private static int maxDepth(String s) {
+        int ans = 0;
+        int tmp = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '(')
+                tmp++;
+            else if(ch == ')')
+                tmp--;
+
+            ans = tmp > ans ? tmp : ans;
+        }
+
+        return ans;
+    }
+}
