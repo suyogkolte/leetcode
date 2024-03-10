e58198213a97426df8313aaf19f10d422ec1d8bf
suyogkolte <suyogkolte1989@gmail.com>
2024-03-10 14:58:52 -0400

    Create Kth_Largest_Element.java
    Code to return the kth largest element in an array without sorting

---
+++ Algorithms/Heaps/src/Kth_Largest_Element.java
@@ -0,0 +1,25 @@
+// Source: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+import java.util.PriorityQueue;
+
+public class Kth_Largest_Element {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        int res = findKthLargest(nums, k);
+        System.out.println(k + " largest element is " + res);
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for(int num : nums) {
+            heap.add(num);
+
+            if(heap.size() > k)
+                heap.remove();
+        }
+
+        return heap.peek();
+    }
+}
