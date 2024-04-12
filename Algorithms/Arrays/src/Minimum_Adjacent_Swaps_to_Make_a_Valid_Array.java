e2b8de0d5ae7fe96ade7af6867923a8c827222d4
suyogkolte <suyogkolte1989@gmail.com>
2024-04-12 08:34:56 -0400

    Create Minimum_Adjacent_Swaps_to_Make_a_Valid_Array.java
    Code to find the minimum number of swaps needed to make an array valid

---
+++ Algorithms/Arrays/src/Minimum_Adjacent_Swaps_to_Make_a_Valid_Array.java
@@ -0,0 +1,23 @@
+//Source: https://leetcode.com/problems/minimum-adjacent-swaps-to-make-a-valid-array/description/
+
+public class Minimum_Adjacent_Swaps_to_Make_a_Valid_Array {
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,5,3,1};
+
+        int minIdx = 0;
+        int maxIdx = nums.length - 1;
+
+        for (int i = 1; i < nums.length; i++){
+            if(nums[i] < nums[minIdx])
+                minIdx = i;
+            if(nums[i] >= nums[maxIdx])
+                maxIdx = i;
+        }
+
+        int minSwaps = minIdx + (nums.length - 1 - maxIdx);
+        if(minIdx > maxIdx)
+            minSwaps = minSwaps - 1;
+
+        System.out.println("Minimum number of swaps needed to make a valid array is: " + minSwaps);
+    }
+}
