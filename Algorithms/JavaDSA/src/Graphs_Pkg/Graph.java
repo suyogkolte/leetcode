ec4f282e25211ba9af1084abfbe255ff2743d720
suyogkolte <suyogkolte1989@gmail.com>
2024-05-14 07:35:53 -0400

    Update Graph.java
    Added logic to include Bipartite graph and Kahn's algorithm

--- Algorithms/JavaDSA/src/Graphs_Pkg/Graph.java
+++ Algorithms/JavaDSA/src/Graphs_Pkg/Graph.java
@@ -1,10 +1,8 @@
 package Graphs_Pkg;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Graph {
     private int vertices; //   Total number of vertices possible
@@ -108,20 +106,117 @@ public class Graph {
         System.out.println("Shortest distance between " + src + " and " + dest + " is " + distance[dest]);
     }
 
+    public boolean isBipartite(){
+        // this array will store the color assigned for evey node in the graph
+        int[] colors = new int[vertices];
+
+        Arrays.fill(colors, -1); // initialise all vertices with no color
+
+        for(int i = 0; i < vertices; i++){
+            if(colors[i] == -1){
+                if(!bipartitieBFS(i, colors))
+                    return false; // If BFS on this component is not bipartite then return false
+            }
+        }
+        return true; // if BFS on all components is bipartite, return true
+    }
+
+    private boolean bipartitieBFS(int src, int[] colors){
+        // Two colors == 0 and 1
+        Queue<Integer> q = new LinkedList<>();
+        colors[src] = 0;
+        q.add(src);
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int neighbor : adj[node]){
+                if(colors[neighbor] == -1){
+                    q.add(neighbor);
+                    // if the neighbor has not been colored then assign the opposite color
+                    colors[neighbor] = 1 - colors[node];
+                }
+                else if(colors[neighbor] == colors[node]){
+                    // if the neighbor has same colo as the current node
+                    // then the graph is not bipartite
+                    return false;
+                }
+            }
+        }
+
+        //if the bfs traversal of this component is bipartite then return true
+        return true;
+    }
+
+    // M edges
+    // N nodes
+    // Kahn's algo time complexity: O(M + N)
+    // space complexity: O(N)
+    public List<Integer> topologicalSort(){
+        //Kahn's algorithm
+
+        List<Integer> result = new ArrayList<>();
+
+        int[] inDegree = new int[vertices];
+
+        // calculating in-degree for all vertices: O(M)
+        for(int i = 0; i < vertices; i++){
+            for(int neighbor : adj[i]){
+                inDegree[neighbor]++;
+            }
+        }
+
+        // Create a queue to store vertices with in-degree 0: O(N)
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < vertices; i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+
+        // O(N)
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            result.add(node);
+
+            for(int neighbor : adj[node]){
+                inDegree[neighbor]--;
+                if(inDegree[neighbor] == 0){
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        if(result.size() != vertices){
+            //Graph has a cycle, so topological sorting is not possible
+            return new ArrayList<>();
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        Graph g = new Graph(6);
-        g.addEdge(0, 1, true);
-        g.addEdge(1, 3, true);
-        g.addEdge(1, 2, true);
-        g.addEdge(0, 2, true);
+//        Graph g = new Graph(6);
+//        g.addEdge(0, 1, true);
+//        g.addEdge(1, 3, true);
+//        g.addEdge(1, 2, true);
+//        g.addEdge(0, 2, true);
         //g.printAdjList();
 
         //g.dfs(0);
         //g.bfs(0);
 
-        g.addEdge(4, 5, true);
+        //g.addEdge(4, 5, true);
         //System.out.println("The number of connected components is " + g.connectedComponents());
 
-        g.shortestPath(0, 3);
+        //g.shortestPath(0, 3);
+        //System.out.println(g.isBipartite());
+
+        Graph g = new Graph(4);
+        g.addEdge(0, 1, false);
+        g.addEdge(1, 3, false);
+        g.addEdge(1, 2, false);
+        g.addEdge(0, 2, false);
+        System.out.println(g.topologicalSort());
     }
 }
